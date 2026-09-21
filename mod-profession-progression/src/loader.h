76cef06b20695aa19207afece9ae8213293bca2b
Kirill <kd@ekf.ru>
2026-09-21 15:19:27 +0300

    Add files via upload

---
+++ mod-profession-progression/src/loader.h
@@ -0,0 +1,7 @@
+#ifndef _MOD_PROFESSION_PROGRESSION_LOADER_H
+#define _MOD_PROFESSION_PROGRESSION_LOADER_H
+
+// Name must match the module folder (mod-profession-progression).
+void Addmod_profession_progressionScripts();
+
+#endif
