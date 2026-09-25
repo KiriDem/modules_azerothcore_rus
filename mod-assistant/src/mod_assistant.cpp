2304acf677c766addc66f29c938be6e048369168
Kirill <kd@ekf.ru>
2026-09-25 13:39:39 +0300

    Delete mod-assistant/src directory

--- mod-assistant/src/mod_assistant.cpp
+++
@@ -1,8 +0,0 @@
-#include "mod_assistant.h"
-
-Assistant::Assistant() : CreatureScript("npc_assistant"), WorldScript("AssistantWorldScript") {}
-
-void Addmod_assistantScripts()
-{
-    new Assistant();
-}
